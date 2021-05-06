5a74632ca0a7612c954fd46702dfa335f2426aff
patsub <pkostrzewski1@gmail.com>
2021-05-07 00:57:54 +0200

    Uproszczona wersja do oceny

--- src/App.js
+++ src/App.js
@@ -3,27 +3,25 @@ import Playfair from './Playfair';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
-import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+// import Button from '@material-ui/core/Button';
 import Switch from '@material-ui/core/Switch';
 import "@fontsource/roboto" // Defaults to weight 400.
+import { Paper } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
   body: {
-    backgroundColor: '#ebebeb',
     fontFamily: 'Roboto',
     display: 'flex',
     flexDirection: 'column',
-    flex: 1,
-    height: '100vh'
   },
   header: {
     textAlign: 'center'
   },
   userText: {
-    width: '100%'
+    width: '100%',
+    marginBottom: '1rem'
   }
 }));
 
@@ -35,66 +33,50 @@ const allowedChars = [
   'y', 'z', '1', '2', '3', '4',
   '5', '6', '7', '8', '9', '0',
   ' ', ',', '.', ':', '?', '!'
-]
-const playfair = new Playfair();
+];
+
+const playfair = new Playfair(allowedChars);
+
+const filterString = string => {
+  return string
+    .toLowerCase()
+    .split('')
+    .filter(char => allowedChars.includes(char))
+    .join('');
+};
 
 const App = () => {
 
   const classes = useStyles();
-  const [displayPlayfairPlusMatrix, setDisplayPlayfairPlusMatrix] = React.useState(false);
   const [decryptMode, setDecryptMode] = React.useState(false);
   const [cipherInput, setCipherInput] = React.useState('');
+  const [cipherOutput, setCipherOutput] = React.useState('');
+  const [cipherOutputPairs, setCipherOutputPairs] = React.useState(playfair.duples);
   const [cipherKey, setCipherKey] = React.useState('');
   const [cipherKeyWithoutRepetition, setCipherKeyWithoutRepetition] = React.useState('');
-  const [matrixString, setMatrixString] = React.useState(playfair.matrixString);
-
-  console.log(matrixString);
+  const [matrixArray, setMatrixArray] = React.useState(playfair.matrixArray);
 
   const handleSwitchDecryptMode = (event) => {
     setDecryptMode(event.target.checked);
+    console.log(event.target.checked);
+    setCipherOutput(playfair.process({ input: cipherInput, decrypt: event.target.checked }));
   };
 
   const handleCipherInputChange = (event) => {
-    setCipherInput(event.target.value);
+    let input = filterString(event.target.value);
+    setCipherInput(input);
+    setCipherOutput(playfair.process({ input: input, decrypt: decryptMode }));
+    setCipherOutputPairs(playfair.duples);
   };
 
   const handleCipherKeyChange = (event) => {
-    setCipherKey(event.target.value);
-    playfair.setKey(event.target.value);
+    const key = filterString(event.target.value);
+    setCipherKey(key);
+    playfair.setKey(key);
     setCipherKeyWithoutRepetition(playfair.keyWithoutRepetition);
-    setMatrixString(playfair.matrixString)
+    setMatrixArray(playfair.matrixArray);
+    setCipherOutput(playfair.process({ input: cipherInput, decrypt: decryptMode }));
   };
-
-  const playfairPlusMatrixQuestion = (
-    <Paper style={{ width: '100%', height: '4rem', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-      <Button
-        variant="contained"
-        color="primary"
-        onClick={() => setDisplayPlayfairPlusMatrix(!displayPlayfairPlusMatrix)}
-      >
-        Primary
-      </Button>
-    </Paper>
-  );
-
-  const playfairPlusMatrix = allowedChars.map((value, key) => {
-
-    return (
-      <Grid key={key} item xs={2}>
-        <Paper className={classes.paper} style={{
-          height: '4rem',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center'
-        }}>
-          {value === ' ' ?
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M18 9v4H6V9H4v6h16V9z" /></svg>
-            : value
-          }
-        </Paper>
-      </Grid>
-    )
-  });
 
   return (
     <div className={classes.body}>
@@ -107,58 +89,111 @@ const App = () => {
         <main>
           <Grid container spacing={3}>
             <Grid item xs={6}>
-              <Paper>
-                <Typography component="div">
+              <Paper className={'paper'} elevation={2}>
+                {/* <Typography component="div">
                   <Grid component="label" container alignItems="center" spacing={1}>
                     <Grid item>Szyfruj</Grid>
                     <Grid item>
-                      <Switch checked={decryptMode} onChange={handleSwitchDecryptMode} name="checkedC" />
+                      <Switch
+                        color="primary"
+                        checked={decryptMode}
+                        onChange={handleSwitchDecryptMode}
+                      />
                     </Grid>
                     <Grid item>Deszyfruj</Grid>
                   </Grid>
-                </Typography>
+                </Typography> */}
                 <TextField
                   className={classes.userText}
-                  label="Twój tekst"
+                  label="Twój tekst (Wejście)"
                   multiline
                   rows={4}
                   variant="outlined"
                   value={cipherInput}
                   onChange={handleCipherInputChange}
                 />
-                <Typography variant="body2" gutterBottom>
+                {/* <Typography variant="body2" gutterBottom>
                   Text może zawierać tylko znaki z alfabetu łacińskiego od A do Z, liczby od 0 do 9 oraz spacje ' '.
-                </Typography>
+                </Typography> */}
                 <TextField
+                  className={classes.userText}
                   required
                   id="outlined-required"
                   label="Klucz szyfrujący"
                   value={cipherKey}
                   onChange={handleCipherKeyChange}
                   variant="outlined"
-                  inputProps={{ maxLength: 36 }}
                 />
-                <Typography variant="body2" gutterBottom>
+                {/* <Typography component="div" style={{ marginBottom: '1rem' }}>
+                  <Grid component="label">
+                    <Button variant="contained" color="primary">
+                      {decryptMode ? 'Deszyfruj' : 'Szyfruj'}
+                    </Button>
+                  </Grid>
+                </Typography> */}
+                {/* <Typography variant="body2" gutterBottom>
                   Text może zawierać tylko znaki z alfabetu łacińskiego od A do Z, liczby od 0 do 9 oraz spacje ' '.
-                </Typography>
+                </Typography> */}
                 <TextField
                   className={classes.userText}
                   label="Wyjście"
                   multiline
                   rows={4}
                   variant="outlined"
-                  // value={userText}
-                  // onChange={handleChange}
+                  disabled={true}
+                  value={cipherOutput}
                 />
               </Paper>
             </Grid>
             <Grid item xs={6}>
-              <Paper>Wejście: {cipherInput}</Paper>
-              <Paper>Klucz: {cipherKey}</Paper>
-              <Paper>Klucz bez powtórzeń: {cipherKeyWithoutRepetition}</Paper>
+              <Paper className={'paper'} style={{ marginBottom: '1rem' }} elevation={2}>
+                <div className={'cryptoInfo'}>
+                  Wejście:
+                  <p>
+                    {cipherOutputPairs.length > 0 && cipherOutputPairs.map((value, key) => (
+                      <span key={key} className="cipherOutputPair">[{value[0]},{value[1]}]</span>
+                    ))}
+                  </p>
+                </div>
+                <div className={'cryptoInfo'}>
+                  Klucz:
+                  <p>
+                    {cipherKey}
+                  </p>
+                </div>
+                <div className={'cryptoInfo'}>
+                  Klucz bez powtórzeń:
+                  <p>
+                    {cipherKeyWithoutRepetition.length > 0 && (
+                      <span className={'cipherKeyWithoutRepetition'}>
+                        {cipherKeyWithoutRepetition}
+                      </span>
+                    )}
+                  </p>
+                </div>
+              </Paper>
               <Grid container className={classes.root} spacing={1}>
 
-                {displayPlayfairPlusMatrix ? playfairPlusMatrix : playfairPlusMatrixQuestion}
+                {matrixArray.map((value, key) => {
+                  return (
+                    <Grid key={key} item xs={2}>
+                      <Paper elevation={2} className={'paper'} style={{
+                        height: '4rem',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        padding: 'unset',
+                        border: 'unset',
+                        backgroundColor: (cipherKeyWithoutRepetition.includes(value) ? 'orange' : 'transparent')
+                      }}>
+                        {value === ' ' ?
+                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M18 9v4H6V9H4v6h16V9z" /></svg>
+                          : value
+                        }
+                      </Paper>
+                    </Grid>
+                  )
+                })}
 
               </Grid>
 
--- src/Playfair.js
+++ src/Playfair.js
@@ -2,18 +2,12 @@ class Playfair {
     allowedChars;
     matrixString;
     matrix;
+    duples;
     
-    constructor() {
-        this.allowedChars = [
-            'a', 'b', 'c', 'd', 'e', 'f',
-            'g', 'h', 'i', 'j', 'k', 'l',
-            'm', 'n', 'o', 'p', 'q', 'r',
-            's', 't', 'u', 'v', 'w', 'x',
-            'y', 'z', '1', '2', '3', '4',
-            '5', '6', '7', '8', '9', '0',
-            ' ', ',', '.', ':', '?', '!'
-        ];
-        this.matrixString = [...this.allowedChars].join('');
+    constructor(allowedChars) {
+        this.allowedChars = allowedChars;
+        this.matrixArray = this.allowedChars;
+        this.duples = [];
 
         for (let i = 0; i < this.allowedChars; i += 7) {
             this.matrix.push(this.allowedChars.slice(i, i + 6));
@@ -22,28 +16,20 @@ class Playfair {
 
     setKey(key) {
       if (key) {
-        let sanitizedKey = key.toLowerCase().split('').map((char) => {
-            if(this.allowedChars.includes(char))
-                return char;
-            else
-                return '';
-            
-        });
-        sanitizedKey = [...sanitizedKey].join('');
-
-        const keyWithoutRepetition = new Set(sanitizedKey);
+        const keyWithoutRepetition = new Set(key);
         this.keyWithoutRepetition = [...keyWithoutRepetition].join('');
 
-        const matrixString = [...new Set(`${sanitizedKey}${this.allowedChars}`)];
-        this.matrixString = matrixString;
+        const matrixArray = [...new Set(`${key}${this.allowedChars.join('')}`)];
+        this.matrixArray = matrixArray;
 
         this.matrix = [];
-        for (let i = 0; i < matrixString.length; i += 7) {
-          this.matrix.push(matrixString.slice(i, i + 6));
+        for (let i = 0; i < matrixArray.length; i += 6) {
+          this.matrix.push(matrixArray.slice(i, i + 6));
         }
       } else {
-        const matrixString = [...this.allowedChars].join('');
-        this.matrixString = matrixString;
+        const matrixArray = this.allowedChars;
+        this.matrixArray = matrixArray;
+        this.keyWithoutRepetition = '';
 
           for (let i = 0; i < this.allowedChars; i += 7) {
             this.matrix.push(this.allowedChars.slice(i, i + 6));
@@ -52,27 +38,28 @@ class Playfair {
     }
     preProcess({ input, decrypt }) {
       // split into duples, fixing double-letters (hello => he lx lo) and padding
-      const text = input.toLowerCase().replace(/[^a-z]/g, '').replace(/j/g, 'i').split('').filter(x => x !== ' ');
+      const text = input.split('');
       const duples = [];
       for (let i = 0; i < text.length; i += 2) {
         const currentDuple = text.slice(i, i + 2);
         if (!decrypt && currentDuple.length !== 2) {
           currentDuple.push('x');
           duples.push(currentDuple);
-        } else if (!decrypt && currentDuple[0] === currentDuple[1]) {
-          text.splice(i + 1, 0, 'x');
-          duples.push(text.slice(i, i + 2));
+        // @TODO 
+        // } else if (!decrypt && currentDuple[0] === currentDuple[1]) {
+        //   text.splice(i + 1, 0, 'x');
+        //   duples.push(text.slice(i, i + 2));
         } else {
           duples.push(currentDuple);
         }
       }
-  
+      this.duples = duples;
       // find row and column for each letter in duple
       const coordinates = [];
       duples.forEach((duple) => {
         coordinates.push(duple.map((letter) => {
           let col;
-          const row = this.grid.findIndex(row => {
+          const row = this.matrix.findIndex(row => {
             const rowIdx = row.findIndex(x => x === letter);
             if (rowIdx >= 0) {
               col = rowIdx;
@@ -83,18 +70,17 @@ class Playfair {
           return [row, col];
         }));
       });
-  
       return coordinates;
     }
     process({ input, decrypt }) {
-      if (!this.grid) return 'First set the key!';
-      if (input && decrypt && input.length % 2 !== 0) return 'Invalid ciphertext';
+      if (!this.matrix) return 'Najpierw wprowadź klucz!';
+      if (input && decrypt && input.length % 2 !== 0) return 'Nieprawidłowy zaszyfrowany tekst';
       const coordinates = this.preProcess({ input, decrypt });
-  
+
       // set modifiers to respond appropriately based on decrypt switch
-      const modifier = decrypt ? -1 : 1;
-      const wall = decrypt ? 0 : 4;
-      const phase = decrypt ? 4 : -4;
+      // const modifier = decrypt ? -1 : 1;
+      // const wall = decrypt ? 0 : 4;
+      // const phase = decrypt ? 4 : -4;
   
       const processedLocs = [];
       coordinates.forEach((loc) => {
@@ -103,19 +89,28 @@ class Playfair {
   
         let modifiedLoc = [];
   
-        // handle letters on the same row
-        if (loc[0][0] === loc[1][0]) {
-          // increment/decrement the column
-          modifiedLoc[0] = loc[0][1] === wall ? [loc[0][0], wall + phase] : [loc[0][0], loc[0][1] + modifier];
-          modifiedLoc[1] = loc[1][1] === wall ? [loc[1][0], wall + phase] : [loc[1][0], loc[1][1] + modifier];
-          return processedLocs.push(modifiedLoc);
-        }
+        // @TODO make it work 
+        // // handle letters on the same row
+        // if (loc[0][0] === loc[1][0]) {
+        //   // increment/decrement the column
+        //   modifiedLoc[0] = loc[0][1] === wall ? [loc[0][0], wall + phase] : [loc[0][0], loc[0][1] + modifier];
+        //   modifiedLoc[1] = loc[1][1] === wall ? [loc[1][0], wall + phase] : [loc[1][0], loc[1][1] + modifier];
+        //   return processedLocs.push(modifiedLoc);
+        // }
   
+        // // handle letters in the same column
+        // if (loc[0][1] === loc[1][1]) {
+        //   // increment/decrement the row
+        //   modifiedLoc[0] = loc[0][0] === wall ? [wall + phase, loc[0][1]] : [loc[0][0] + modifier, loc[0][1]];
+        //   modifiedLoc[1] = loc[1][0] === wall ? [wall + phase, loc[1][1]] : [loc[1][0] + modifier, loc[1][1]];
+        //   return processedLocs.push(modifiedLoc);
+        // }
+
         // handle letters in the same column
         if (loc[0][1] === loc[1][1]) {
           // increment/decrement the row
-          modifiedLoc[0] = loc[0][0] === wall ? [wall + phase, loc[0][1]] : [loc[0][0] + modifier, loc[0][1]];
-          modifiedLoc[1] = loc[1][0] === wall ? [wall + phase, loc[1][1]] : [loc[1][0] + modifier, loc[1][1]];
+          modifiedLoc[0] = [loc[1][0], loc[1][1]];
+          modifiedLoc[1] = [loc[0][0], loc[0][1]];
           return processedLocs.push(modifiedLoc);
         }
   
@@ -124,10 +119,10 @@ class Playfair {
         modifiedLoc[1] = [loc[1][0], loc[0][1]];
         processedLocs.push(modifiedLoc);
       });
-  
+
       // translate coordinates into ciphertext
       const processedText = processedLocs
-        .map((loc) => [this.grid[loc[0][0]][loc[0][1]], this.grid[loc[1][0]][loc[1][1]]].join(''))
+        .map((loc) => [this.matrix[loc[0][0]][loc[0][1]], this.matrix[loc[1][0]][loc[1][1]]].join(''))
         .join('');
   
       return processedText.toUpperCase();
